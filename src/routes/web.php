f5365f46d8e3ace72602095894898e2e407a4481
koko-chii <kokochii@outlook.jp>
2026-02-25 12:34:47 +0900

    Todo一覧表示機能とレイアウトの実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
+use App\Http\Controllers\TodoController; // 先頭に追記
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [TodoController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
