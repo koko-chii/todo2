b8261b5a867394a78d24700692a67758232713cd
koko-chii <kokochii@outlook.jp>
2026-02-25 15:11:27 +0900

    更新削除機能を実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\TodoController; // 先頭に追記
 use Illuminate\Support\Facades\Route;
 
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
 Route::get('/', [TodoController::class, 'index']);
 Route::post('/todos', [TodoController::class, 'store']);
