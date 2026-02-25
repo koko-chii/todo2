cc222dc34a36a2764d0fab856f4cfbe1b5ba55d7
koko-chii <kokochii@outlook.jp>
2026-02-25 14:12:35 +0900

    セッションとバリデーションの実施

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
 
 public function index()
     {
-        return view('index');
+
+$todos = Todo::all();
+return view('index', compact('todos'));
+    }
+
+public function store(TodoRequest $request)
+    {
+    $todo = $request->only(['content']);
+    Todo::create($todo);
+
+    return redirect('/')->with('message', 'Todoを作成しました');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\TodoController; // 先頭に追記
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'index']);
-
+Route::post('/todos', [TodoController::class, 'store']);
